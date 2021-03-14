54c4c89f268c28b93e798cf300cd84d912a51c28
Sayed Muhammad Idrees <sayedidrees47@gmail.com>
2021-03-14 23:43:57 +0500

    Add project files.

---
+++ Sys/Html.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CryptoGenesis_Core.Sys
+{
+    public static class Html
+    {
+        public static string IsActive(this IHtmlHelper htmlHelper, string controller, string action)
+        {
+            var routeData = htmlHelper.ViewContext.RouteData;
+
+            var routeAction = routeData.Values["action"].ToString();
+            var routeController = routeData.Values["controller"].ToString();
+
+            var returnActive = (controller == routeController && (action == routeAction || routeAction == "Details"));
+
+            return returnActive ? "active" : "";
+        }
+    }
+}
---
+++ wwwroot/js/main.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+
+});
+
+
+
+$('#txtEnter,#txtClose,#txtInvested').on('input', function(e) {
+    calculateProfit();
+});
+
+
+var calculateProfit = function() {
+
+    var enter = $("#txtEnter").val();
+    var close = $("#txtClose").val();
+    var invested = $("#txtInvested").val();
+
+
+    var check1 = $.isNumeric(enter);
+    var check2 = $.isNumeric(close);
+    var check3 = $.isNumeric(invested);
+
+    if (check1 && check2 && check3) {
+        var purchaseCoin = invested / enter;
+        var closingOrder = purchaseCoin * close;
+        var PL = closingOrder - invested;
+
+        $('#lblCoins').text('Total Coins : ' + purchaseCoin.toFixed(5));
+        $('#lblInvested').text('Total Invest : ' + invested);
+        var PLText = "Total Profit/Loss : ";
+        if (PL > 0) {
+            PLText = PLText + '+'
+            $("#lblProfit").css("color", "green");
+        } else {
+            PLText = PLText + '-'
+            $("#lblProfit").css("color", "red");
+        }
+
+        $('#lblProfit').text(PLText + PL.toFixed(2));
+    }
+
+};
+
+function validate(evt) {
+
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+
+    // alert(charCode + ' ' + evt.which);
+    if ((charCode >= 48 && charCode <= 57) || charCode == 46 || charCode == 0)
+        return true;
+    return false;
+}
